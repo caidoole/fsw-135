682d646bb883ec0277ba456b9f7ffb2a6eba7c82
caidoole <caitlin.dooley@bryanuniversity.edu>
2021-05-04 09:20:24 -0700

    RTV Part 4

---
+++ wk6/rockthevote/client/src/components/IssueList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Issue from './Issue.js'
+
+export default function IssueList(props) {
+  const { issues } = props
+  const issueData = issues.map(issue => {
+    return (
+      <Issue
+        {...issue}
+        key={issue._id}
+      />
+    )
+  })
+  return (
+    <div className="issue-list">
+      {issueData}
+    </div>
+  )
+}
+
+
