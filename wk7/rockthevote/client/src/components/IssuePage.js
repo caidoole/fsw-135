7cdd05f9a27a35cf238e5c5f7b82a960ea3d431a
caidoole <caitlin.dooley@bryanuniversity.edu>
2021-05-05 23:39:44 -0700

    135 Capstone

---
+++ wk7/rockthevote/client/src/components/IssuePage.js
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect } from 'react'
+import IssueList from './IssueList.js'
+import { IssueContext } from '../context/IssueProvider.js'
+
+export default function IssuePage() {
+  const {
+    getAllIssues,
+  } = useContext(IssueContext)
+
+  useEffect(() => {
+   getAllIssues()
+  }, []);
+
+  return (
+    <div className="issue-page">
+      <IssueList />
+    </div>
+  )
+}
+
+
+
+
+
